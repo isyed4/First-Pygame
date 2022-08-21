8eac4b6e8211e6b3b4ba93170c8986da3a6cb688
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-08-21 10:48:07 -0500

    added left movement key for red spaceship

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ WHITE = (255, 255, 255)
 
 FPS = 60
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+velocity = 5
 
 # os helps with path finding regardless of operating system
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
@@ -23,16 +24,20 @@ RED_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(
     RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 
-def draw_window():
+def draw_window(red, yellow):
     WIN.fill((WHITE))
     # use blit when you want to draw a surface on the screen
-    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
-    WIN.blit(RED_SPACESHIP_IMAGE, (700, 100))
+    WIN.blit(YELLOW_SPACESHIP_IMAGE, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP_IMAGE, (red.x, red.y))
     pygame.display.update()
 
 
 # pygame event loop, what is going to pop up on screen?
 def main():
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -40,8 +45,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_a]: # LEFT
+            yellow.x -= velocity
 
-        draw_window()
+
+        draw_window(red, yellow)
 
     pygame.quit()
 
